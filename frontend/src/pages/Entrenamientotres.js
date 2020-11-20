d937443b3cb532b5a6f30b638b2f9230f6ff073a
Timoteo <vallejo.timo15@gmail.com>
2020-11-20 08:36:13 -0500

    cambios en el frontend

--- frontend/src/pages/Entrenamientotres.js
+++ frontend/src/pages/Entrenamientotres.js
@@ -15,21 +15,20 @@ class Entrenamiento_tres extends Component {
                 </section>
                 <section className="plann">
                     
-<<<<<<< HEAD
+
                     <button className="unoo" ><Link  className="un">Circuito 1</Link></button>
                     <button className="doss" ><Link className="dos">Circuito 2</Link></button>
                     <button className="tress"><Link className="tres">Circuito 3</Link></button>
                     <button className="cuatroo"><Link className="cuatro">Circuito 4</Link></button>
                    
                     <button className="atras"><Link className="atra" to="/usario">Atras</Link></button>
-=======
                     <button className="unoo" ><Link className="un">Circuito 1<br/><p>Abdomen</p></Link></button>
                     <button className="doss" ><Link className="dos">Circuito 2<br/><p>Brazos</p></Link></button>
                     <button className="tress"><Link className="tres">Circuito 3<br/><p>Piernas</p></Link></button>
                     <button className="cuatroo"><Link className="cuatro">Circuito 4<br/><p>Pecho</p></Link></button>
                     <button className="cincoo"><Link className="cinco">Circuito 5</Link></button>
                     <button className="atras"><Link className="atra" to="/">Inicio</Link></button>
->>>>>>> 31c9795dcb87443716fa98597880eb62b0da63d0
+
                 </section>
 
                 </div>
